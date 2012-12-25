ed4340d2bd15c49cb28a710b758c41c7f9528d39
Patrick Asare <p.asare0@gmail.com>
2012-12-25 16:59:10 -0500

    Hello

---
+++ Zion America/Login_ViewController.m
@@ -0,0 +1,38 @@
+//
+//  Login_ViewController.m
+//  Zion America
+//
+//  Created by Patrick Asare on 12/25/12.
+//  Copyright (c) 2012 Patrick Asare. All rights reserved.
+//
+
+#import "Login_ViewController.h"
+
+@interface Login_ViewController ()
+
+@end
+
+@implementation Login_ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
